482df1c3c2eaac86c2121b3547bf4cd18f8f4528
José Santos <josemfsantos97@gmail.com>
2025-12-11 19:37:13 +0100

    updated dag

--- dags/dummy.py
+++ dags/dummy.py
@@ -1,25 +1,23 @@
-from datetime import datetime, timedelta
-from airflow import DAG
-from airflow.operators.python import PythonOperator
+import datetime
 
-# Dummy function
-def print_hello():
-    print("Hello from Airflow!")
+import pendulum
 
-# Define the DAG
+from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.providers.standard.operators.latest_only import LatestOnlyOperator
+from airflow.sdk import DAG, TriggerRule
+
 with DAG(
-    dag_id="dummy_dag",
-    description="A simple dummy DAG for testing",
-    schedule_interval=timedelta(days=1),
-    start_date=datetime(2025, 12, 11),
+    dag_id="latest_only_with_trigger",
+    schedule=datetime.timedelta(hours=4),
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
-    tags=["test"],
+    tags=["example3"],
 ) as dag:
+    latest_only = LatestOnlyOperator(task_id="latest_only")
+    task1 = EmptyOperator(task_id="task1")
+    task2 = EmptyOperator(task_id="task2")
+    task3 = EmptyOperator(task_id="task3")
+    task4 = EmptyOperator(task_id="task4", trigger_rule=TriggerRule.ALL_DONE)
 
-    task_hello = PythonOperator(
-        task_id="say_hello",
-        python_callable=print_hello,
-    )
-
-    # You can chain tasks if you add more
-    task_hello
+    latest_only >> task1 >> [task3, task4]
+    task2 >> [task3, task4]
